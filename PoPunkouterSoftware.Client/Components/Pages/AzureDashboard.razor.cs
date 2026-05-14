7ef5b8a0c95d7f17ef12d7f185174798df77a168
punkouter26 <punkouter26@gmail.com>
2026-05-13 22:24:43 -0400

    chore: update gitignore to exclude root screenshots; update azure report data; fix AzureDashboard routing import

--- PoPunkouterSoftware.Client/Components/Pages/AzureDashboard.razor.cs
+++ PoPunkouterSoftware.Client/Components/Pages/AzureDashboard.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.JSInterop;
 using PoPunkouterSoftware.Shared.Azure;
